54f04ea50e31e8220fac00cd46385d6552012e83
your name <you@email.com>
2026-07-30 11:20:25 +0100

    push

--- database/seeders/AdminWalletSeeder.php
+++ database/seeders/AdminWalletSeeder.php
@@ -2,18 +2,27 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\AdminWallet;
 
 class AdminWalletSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-  public function run(): void
-{
-    $this->call([
-        AdminWalletSeeder::class,
-    ]);
-}
+    public function run(): void
+    {
+        AdminWallet::updateOrCreate(
+            [
+                'wallet_number' => 'TNK-XAF-001',
+            ],
+            [
+                'wallet_name' => 'Tunko Main Wallet',
+                'currency' => 'XAF',
+                'balance' => 10000000.00,
+                'status' => 'active',
+                'description' => 'Main settlement wallet for manual deposits',
+            ]
+        );
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,10 +9,11 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-   public function run(): void
+public function run(): void
 {
     $this->call([
         AdminSeeder::class,
+        AdminWalletSeeder::class,
     ]);
 }
     
